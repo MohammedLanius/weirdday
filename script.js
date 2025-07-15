7f023e4401e4d31faf82c9fa6795e840aa5600e0
MohammedLanius <mohammedno1@proton.me>
2025-07-15 19:03:53 +0200

    upload projec

---
+++ script.js
@@ -0,0 +1,57 @@
+let day = document.querySelector('.day'); let dotInterval;
+const dayVariants = {
+    "الأحد": ["Sun-dazed", "Sun-donut", "Sun-dumb"],
+    "الإثنين": ["Mon-depressed", "Moan-day", "Mondoom"],
+    "الثلاثاء": ["Tues-donkey", "Twos-day", "Tuna-slay"],
+    "الأربعاء": ["Wednes-dazed", "Wet-nosed", "Wend-snake"],
+    "الخميس": ["Thirst-day", "Thunderthigh", "Thuds-day"],
+    "الجمعة": ["الجمعة"],
+    "السبت": ["Satur-doom", "Sadder-day", "Sat-on-dog"]
+};
+
+function startLoadingText() {
+    let dots = ".";
+    dotInterval = setInterval(() => {
+        dots = dots.length >= 3 ? "." : dots + ".";
+        day.textContent = dots;
+    }, 500);
+}
+
+function stopLoadingText(newText) {
+    clearInterval(dotInterval);
+    day.textContent = newText;
+}
+async function gettimeprayer() {
+    try {
+        const url = new URL("https://api.aladhan.com/v1/timingsByCity");
+        url.searchParams.set("country", "africa");
+        url.searchParams.set("city", "tripoly");
+        url.searchParams.set("method", "8");
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data.data.date.hijri.weekday.ar;
+
+    } catch (error) {
+        console.error("حدث خطأ في جلب مواقيت الصلاة:", error);
+    }
+
+}
+async function weirdday() {
+    startLoadingText();
+    let dayName = await getday();
+    let wiredtoday = dayVariants[dayName][Math.floor(Math.random() * dayVariants[dayName].length)]
+    stopLoadingText(wiredtoday);
+
+
+
+
+}
+async function getday() {
+    return gettimeprayer();
+
+
+}
+weirdday()
